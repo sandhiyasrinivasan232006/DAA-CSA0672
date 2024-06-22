10b3c9426dcc80ecec781d4f9c118b6b13afdc6c
sandhiyasrinivasan232006 <166091294+sandhiyasrinivasan232006@users.noreply.github.com>
2024-06-22 14:21:19 +0530

    Add files via upload

---
+++ binomial coefficient.py
@@ -0,0 +1,132 @@
+def binomial_coefficient(n, k):
+    if k == 0 or k == n:
+        return 1
+    else:
+        return binomial_coefficient(n-1, k-1) + binomial_coefficient(n-1,k)
+n = 5
+k = 2
+print(binomial_coefficient(n, k))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def binomial_coefficient(n, k):
+    if k == 0 or k == n:
+        return 1
+    else:
+        return binomial_coefficient(n-1, k-1) + binomial_coefficient(n-1,k)
+n = 5
+k = 2
+print(binomial_coefficient(n, k))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
